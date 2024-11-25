2ef01088090bc205eb5a8670e73ee16d19646a1e
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-11-25 21:53:39 +0800

    Completed async module for getting data from api

---
+++ src/displayController.js
@@ -0,0 +1,7 @@
+export function displayCurrentCity(content) {
+  document.querySelector("#current-city").textContent = content;
+}
+
+export function displayCurrentTemp(content) {
+  document.querySelector("#current-temp").textContent = content;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+import { displayCurrentCity, displayCurrentTemp } from "./displayController";
+
+async function getData(location) {
+  const apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=WSVCK7JA9HKPVCEEDR8C97F99&contentType=json`;
+
+  await fetch(apiURL, { mode: "cors" })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      displayData(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function displayData(forecast) {
+  console.log(forecast);
+  displayCurrentCity(forecast.resolvedAddress);
+  displayCurrentTemp(forecast.currentConditions.temp);
+}
+
+// getData("Cebu,Philippines");
